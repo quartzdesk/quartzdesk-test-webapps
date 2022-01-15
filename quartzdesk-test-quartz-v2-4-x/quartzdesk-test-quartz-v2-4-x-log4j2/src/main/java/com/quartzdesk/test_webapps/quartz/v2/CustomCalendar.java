113eb49e6c840b381f984ddc5309b4b2f23a350f
xmoravej <jan.moravec@quartzdesk.com>
2022-01-15 17:07:29 +0100

    Added 4.0.9 changes.

---
+++ quartzdesk-test-quartz-v2-4-x/quartzdesk-test-quartz-v2-4-x-log4j2/src/main/java/com/quartzdesk/test_webapps/quartz/v2/CustomCalendar.java
@@ -0,0 +1,13 @@
+package com.quartzdesk.test_webapps.quartz.v2;
+
+import org.quartz.impl.calendar.BaseCalendar;
+
+/**
+ * A dummy implementation of a custom Quartz calendar.
+ *
+ * @version :$
+ */
+public class CustomCalendar
+    extends BaseCalendar
+{
+}
---
+++ quartzdesk-test-quartz-v2-4-x/quartzdesk-test-quartz-v2-4-x-logback/src/main/java/com/quartzdesk/test_webapps/quartz/v2/TestHolidayCalendar.java
@@ -0,0 +1,8 @@
+package com.quartzdesk.test_webapps.quartz.v2;
+
+/**
+ * @version :$
+ */
+public class TestHolidayCalendar
+{
+}
